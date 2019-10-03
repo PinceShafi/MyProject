07d51b67c38e1a8a55d6335e0822846b9041653d
ShafiUllah <Shafikh559@gmail.com>
2019-10-02 22:43:23 -0700

    login is clear

--- login.php
+++ login.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <title>Login</title>
 </head>
 <body>
@@ -14,24 +15,24 @@
                 <div class="col-sm-5">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="display-4">User Login</h3>
+                            <h3 class="display-4"><i class="fas fa-user-circle"></i> User Login</h3>
                         </div>
                         <div class="card-body">
                             <form action="">
                                         <div class="form-group">
-                                            <label for="email">Users Email</label>
-                                            <input type="email" name="email" placeholder="Enter User Email" class="form-control" autocomplete="off">  
+                                            <label for="email"><i class="fas fa-envelope"></i> Users Email</label>
+                                            <input type="email" name="email" placeholder="Enter User Email" class="form-control form-control-sm"" autocomplete="off" >  
 
                                         </div>
 
                                         <div class="form-group">
-                                            <label for="pass">User Password</label>
-                                            <input type="password" name="pass" placeholder="Enter User Password" class="form-control"> 
+                                            <label for="pass"><i class="fas fa-key"></i> User Password</label>
+                                            <input type="password" name="pass" placeholder="Enter User Password" class="form-control form-control-sm""> 
                                                             
                                         </div>
 
                                         <div class="form-group">
-                                            <button type="submit" class="btn btn-primary" style="width:50%">LOGIN</button>        
+                                            <button type="submit" class="btn btn-primary btn-block" >LOGIN</button>        
                                         </div>
                             </form>
                         </div>
