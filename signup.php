c082b27ecb54e2076cfabeb70ea04d0a2285a628
ShafiUllah <Shafikh559@gmail.com>
2019-10-02 22:42:27 -0700

    signup is clear

--- signup.php
+++ signup.php
@@ -6,46 +6,51 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <title>Signup</title>
 </head>
 <body>
-        <div class="container mt-5" id="login">
-            <div class="row justify-content-center">
-                <div class="col-sm-5">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="display-4">User Signup</h3>
+<div class="container mt-5" id="login">
+    <div class="row justify-content-center">
+        <div class="col-sm-5">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="display-4">
+                        <i class="fas fa-user-circle"></i> User Signup
+                    </h3>
+                </div>
+                <div class="card-body">
+                    <form action="signup.php" method="post">
+                        <div class="form-group">
+                            <label for="name"><i class="fas fa-user"></i> Users Name</label>
+                            <input type="text" name="name" placeholder="Enter User Name" class="form-control form-control-sm"> 
+                            </div>
+
+                        <div class="form-group">
+                            <label for="email"><i class="fas fa-envelope"></i> Users Email</label>
+                            <input type="email" name="email" placeholder="Enter User email" class="form-control form-control-sm" autocomplete="off">     
                         </div>
-                        <div class="card-body">
-                            <form action="">
-                                        <div class="form-group">
-                                            <label for="name">Users Name</label>
-                                            <input type="text" name="name" placeholder="Enter User Name" class="form-control"> 
-                                         </div>
 
-                                        <div class="form-group">
-                                            <label for="email">Users Email</label>
-                                            <input type="email" name="email" placeholder="Enter User email" class="form-control" autocomplete="off">     
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="pass">User Password</label>
-                                            <input type="password" name="pass" placeholder="Enter User Password" class="form-control">                   
-                                        </div>
-
-                                        <div class="form-group">
-                                                    <label for="con-pass">Conform Password</label>
-                                                    <input type="password" name="pass" placeholder="Enter User Conform Password" class="form-control" autocomplete="off">                 
-                                        </div>
-
-                                        <div class="form-group">
-                                            <button type="submit" class="btn btn-success" style="width:50%">SIGNUP</button>
-                                        </div>
-                            </form>
+                        <div class="form-group">
+                            <label for="pass"><i class="fas fa-key"></i> User Password</label>
+                            <input type="password" name="pass" placeholder="Enter User Password" class="form-control form-control-sm">                   
                         </div>
-                    </div>
+
+                        <div class="form-group">
+                            <label for="con-pass"><i class="fas fa-key"></i> Confirm Password</label>
+                            <input type="password" name="pass" placeholder="Enter User Confirm Password" class="form-control form-control-sm" autocomplete="off">              
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" name="signup" class="btn btn-primary btn-block btn-sm">SIGNUP</button>
+                        </div>
+                    </form>
+
+                    <?php require_once 'lib/signup.php'; ?>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 </body>
 </html>
